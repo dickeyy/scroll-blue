f130516e53d751ce236ad0cddeb64c0f5f971dce
Kyle Dickey <kyledickey02@gmail.com>
2024-11-10 02:08:01 +0000

    update logo to svg

--- src/components/sidebars/left-sidebar.tsx
+++ src/components/sidebars/left-sidebar.tsx
@@ -121,7 +121,7 @@ function Logo() {
     return (
         <Link className="mb-4 flex h-12 w-12 items-center justify-center" href="/">
             <Image
-                src="/logo.png"
+                src="/logo.svg"
                 alt="Scroll blue logo"
                 className="transition-transform duration-500 hover:rotate-180"
                 width={40}
