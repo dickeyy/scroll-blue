c057602d67711b011c350c87eff3efb1719d3c2e
Kyle Dickey <kyledickey02@gmail.com>
2024-11-11 21:16:40 +0000

    temp: debug prod logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
 import { auth } from "@/lib/next-auth";
 
 export default auth((req) => {
-    if (!req.auth && req.nextUrl.pathname !== "/sign-in") {
+    if (!req.auth && req.nextUrl.pathname !== "/sign-in" && req.nextUrl.pathname !== "/home") {
         const newUrl = new URL("/home", req.nextUrl.origin);
+        console.log("MIDDLEWARE: redirecting to home page");
         return Response.redirect(newUrl);
     } else if (
         req.auth &&
         (req.nextUrl.pathname === "/sign-in" || req.nextUrl.pathname === "/home")
     ) {
+        console.log("MIDDLEWARE: redirecting to feed page");
         const newUrl = new URL("/", req.nextUrl.origin);
         return Response.redirect(newUrl);
     }
