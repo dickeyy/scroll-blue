fa3b50fc2102f608490eb2736d66f58ee1a72972
Kyle Dickey <kyledickey02@gmail.com>
2024-11-14 02:45:22 +0000

    add alt text dialog

--- src/components/post/embeds/media-grid.tsx
+++ src/components/post/embeds/media-grid.tsx
@@ -1,10 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 import { EmbedVideo, VideoEmbed } from "./video-embed";
 
 interface EmbedImage {
     thumb: string;
-    alt: string;
+    alt?: string;
     aspectRatio?: number;
 }
 
@@ -14,6 +26,16 @@ interface EmbedAssets {
 }
 
 export default function MediaGrid({ media }: { media: EmbedAssets }) {
+    const [selectedAltText, setSelectedAltText] = useState<string | undefined>();
+    const [altDialogOpen, setAltDialogOpen] = useState(false);
+
+    const handleAltClick = (alt: string | undefined) => {
+        if (alt) {
+            setSelectedAltText(alt);
+            setAltDialogOpen(true);
+        }
+    };
+
     return (
         <div
             className={cn(
@@ -22,18 +44,53 @@ export default function MediaGrid({ media }: { media: EmbedAssets }) {
             )}
         >
             {media.images?.map((image: EmbedImage, i: number) => (
-                <img
-                    key={i}
-                    src={image.thumb}
-                    alt={image.alt}
-                    className="rounded-lg w-full h-full object-cover"
-                    style={{
-                        aspectRatio: image.aspectRatio?.toString() ?? "1"
-                    }}
-                />
+                <div key={i} className="relative">
+                    <img
+                        src={image.thumb}
+                        alt={image.alt}
+                        className="rounded-lg w-full h-full object-cover"
+                        style={{
+                            aspectRatio: image.aspectRatio?.toString() ?? "1"
+                        }}
+                    />
+                    {image.alt && (
+                        <Button
+                            size="sm"
+                            variant="outline"
+                            className="absolute bottom-1 left-1 py-1 h-fit text-[0.65rem] px-2"
+                            onClick={() => handleAltClick(image.alt)}
+                        >
+                            ALT
+                        </Button>
+                    )}
+                </div>
             ))}
 
             {media.video && <VideoEmbed video={media.video} />}
+
+            <AltDialog
+                text={selectedAltText}
+                open={altDialogOpen}
+                onClose={() => setAltDialogOpen(false)}
+            />
         </div>
+    );
+}
+
+function AltDialog({ text, onClose, open }: { text?: string; onClose: () => void; open: boolean }) {
+    return (
+        <Dialog open={open} onOpenChange={onClose}>
+            <DialogContent className="space-y-2">
+                <DialogHeader>
+                    <DialogTitle className="text-lg">Image Description</DialogTitle>
+                    <DialogDescription>{text || "No alt text available"}</DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="w-full">
+                    <Button variant="outline" className="w-full" onClick={onClose}>
+                        Close
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     );
 }
